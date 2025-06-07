a32dc0e6cb9753ec5c452d057f991afe9de28483
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-07 12:41:57 +0530

    update auth.middleware.js to handle authentication and authorization

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt, { decode } from "jsonwebtoken";
+import redisClient from "../services/redis.service.js";
+
+export const authUser = async (req, res, next) => {
+    try {
+
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        if(!token){
+            return res.status(401).send({ error: "Unauthorized User "});
+        }
+
+        const isBlacklisted = await redisClient.get(token);
+
+        if(isBlacklisted){
+            res.cookies("token", '');
+
+            return res.status(401).send({ error: "Unauthorized User"});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded
+
+        next()
+        
+    } catch (error) {
+        return res.status(401).send({error: "Unauthorized User"})
+    }
+}
