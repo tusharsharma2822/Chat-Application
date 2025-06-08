947f15637055a7ea4e6cb18c6b004e667a4a3eb8
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-08 19:23:07 +0530

    add project controller logic with validation and service integration

---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,29 @@
+import projectModel from "../models/project.model.js";
+import * as projectService from "../services/project.service.js";
+import userModel from "../models/user.model.js";
+import { validationResult } from "express-validator"
+
+export const createProjectController = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+
+        const { name } = req.body;
+        const loggedInUser = await userModel.findOne({ email: req.user.email });
+        const userId = loggedInUser._id;
+
+        const newProject = await projectService.createProject({ name, userId });
+
+        return res.status(201).json(newProject)
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message)
+    }
+
+}
