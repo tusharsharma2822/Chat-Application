3e2607c3096f86b33fbe49c374cb694abf1a1789
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-11 14:47:09 +0530

    update project.controller.js to handle project-related-requests

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -56,3 +56,34 @@ export const getAllProject = async (req, res) => {
         return res.status(400).json({ error: error.message });
     }
 }
+
+export const addUserToProject = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const { projectId, users } = req.body;
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const project = await projectService.addUserToProjectService({
+            projectId,
+            users, 
+            userId: loggedInUser._id
+        })
+
+        return res.status(200).json({
+            project
+        })
+        
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ error: err.message })
+    }
+}
