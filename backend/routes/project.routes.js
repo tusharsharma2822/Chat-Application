2d14cdc37dd92d7300171ddc76ba338748abf32b
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-11 14:48:22 +0530

    update project.routes.js to handle project-related-routes

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -14,6 +14,14 @@ router.post('/create',
 router.get("/all", 
     authMiddleware.authUser,
     projectController.getAllProject
-)
+);
+
+router.put("/add-user", 
+    authMiddleware.authUser,
+    body("projectId").isString().withMessage("Project ID is required and must be a string."),
+    body("users").isArray({ min: 1 }).withMessage("Users must be a non-empty array.").custom((arr) => arr.every(u => typeof u === "string")).withMessage("Each user must be a string."),
+    projectController.addUserToProject
+);
+
 
 export default router;
