bb8c90c7286e66c67f338e8af9e3c2717ea8af18
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-13 12:19:22 +0530

    Updated AppRoutes.jsx to handle the navigation routes at frontend

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home'
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import DashBoard from '../pages/DashBoard';
+import Project from '../pages/Project';
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
             <Route path='/login' element={ <Login /> } />
             <Route path='/register' element={ <Register /> } />
             <Route path='/dashboard' element={ <DashBoard /> } />
+            <Route path='/project' element={ <Project/> } />
         </Routes>
     </BrowserRouter>
   )
