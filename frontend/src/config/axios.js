11f1660cdd428452068b2157eec2457d8aad8c3f
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-13 12:15:56 +0530

    Updated axios.js to handle the connection with backend

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
-})
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
